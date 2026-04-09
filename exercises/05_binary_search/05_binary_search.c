09d1869e876d6217057de69eeb140694c1281d88
yunjianshijie <2098600899@qq.com>
2026-04-09 23:30:12 +0800

    05_exerises

--- exercises/05_binary_search/05_binary_search.c
+++ exercises/05_binary_search/05_binary_search.c
@@ -17,6 +17,25 @@ int n;
 int binary_search(const char *target_name) {
     // TODO: 在这里添加你的代码
     // I AM NOT DONE
+    // 二分法
+    int left = 0;
+    int right = n - 1;
+
+    while(left <= right){
+        int mid = left + (right - left) / 2;
+        int cmp = strcmp(students[mid].name, target_name);
+
+        if(cmp == 0){
+            return mid;
+        } else if(cmp < 0){
+            left = mid + 1; 
+        } else {
+            right = mid - 1; 
+        }
+    }
+
+    return -1; 
+
 }
 
 int main(void) {
