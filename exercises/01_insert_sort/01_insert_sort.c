6a499a1b664fdd4e29f5d3e5d88234142eb32417
yunjianshijie <2098600899@qq.com>
2026-04-11 11:10:49 +0800

    05_exerises_test

--- exercises/01_insert_sort/01_insert_sort.c
+++ exercises/01_insert_sort/01_insert_sort.c
@@ -8,16 +8,12 @@ typedef struct {
 } Student;
 
 void insertion_sort(Student students[], int n) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
-    // 这里
     Student key;
     for (int i = 1; i < n; i++) {
         key = students[i];
-        int j =i-1;
-        while (j >= 0 && students[j].score < key.score) {
+        int j = i - 1;
+        for (; j >= 0 && students[j].score < key.score; j--) {
             students[j + 1] = students[j];
-            j--;
         }
         students[j + 1] = key;
     }
